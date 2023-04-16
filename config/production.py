9efb8890c6f0bcd92d31e63740682bb2608c630a
k-brahma <k.brahma.nirvana@gmail.com>
2023-04-16 14:52:22 +0900

    email smtp settings.

--- config/production.py
+++ config/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
 DEBUG = False
 
 ALLOWED_HOSTS = [env('ALLOWED_HOSTS'), ]
